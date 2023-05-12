70306bbe521b39214c7b72da1d1aa4b96dc05f3c
apersonius <personius12@gmail.com>
2023-05-11 20:29:16 -0500

    update client package.json, add schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,39 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        getSingleUser: async (_, { user, params }, { res }) => {
+            try {
+                const foundUser = await User.findOne({
+                    $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+                });
+
+                if (!foundUser) {
+                    throw new Error('no user with this id');
+                }
+
+                return foundUser;
+            } catch (error) {
+                res.status(400);
+                throw error;
+            }
+        },
+    },
+    Mutation: {
+        createUser: async () => {
+
+        },
+        login: async () => {
+
+        },
+        saveBook: async () =>{
+
+        },
+        deleteBook: async () => {
+
+        },
+    },
+};
+
+module.exports = resolvers;
