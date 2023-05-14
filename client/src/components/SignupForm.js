de53bf395568d905b56199d4656031e7bd3374e5
apersonius <personius12@gmail.com>
2023-05-14 13:02:28 -0500

    add mutations file and code

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { userMutation } from '@apollo/client';
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
+import { CREATE_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
