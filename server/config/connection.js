262b6820d31301e0fa0d139ba1b30287c9dba3d9
apersonius <personius12@gmail.com>
2023-05-19 13:34:41 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bcbs');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cluster0');
 
 module.exports = mongoose.connection;
